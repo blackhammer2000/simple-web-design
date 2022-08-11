f87ddf21c7cae856ac7d8f0e933740c67db1d093
wawerusamuel <waweruzamuel@gmail.com>
2022-08-11 21:54:25 +0300

    first commit for divisima web design...

---
+++ app.js
@@ -0,0 +1,7 @@
+// const toggleDark = document.querySelector("[data-toggle-dark-mode]");
+// const body = document.querySelector("body");
+
+// toggleDark.addEventListener("click", () => {
+//   body.classList.toggle("dark-mode");
+//   toggleDark.classList.toggle("dark-button");
+// });
